b734cac8d142ab249ede38e71d8817f51d6c640a
Alejandro Juega <polofrito321@gmail.com>
2026-02-08 16:57:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,38 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 
+console.log('--- INICIANDO BOT ---');
+
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        // Esta ruta es la estándar en los servidores de Render/Linux
-        executablePath: '/usr/bin/google-chrome-stable', 
-        headless: true,
+        // En Render, el ejecutable suele estar aquí
+        executablePath: '/usr/bin/google-chrome-stable',
+        headless: true, // IMPORTANTE: siempre true en la nube
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-gpu'
+            '--disable-extensions',
+            '--gpu-sandbox',
+            '--binaries-prefix=/usr/bin'
         ]
     }
 });
+
+client.on('qr', (qr) => {
+    console.log('--- NUEVO QR GENERADO ---');
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('--- ¡BOT CONECTADO Y LISTO! ---');
+});
+
+client.on('auth_failure', msg => {
+    console.error('--- ERROR DE AUTENTICACIÓN ---', msg);
+});
+
+console.log('Conectando a WhatsApp...');
+client.initialize();
+
